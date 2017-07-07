81441e99baa6a2727005addd1c34acdda216548c
XieBiao <xiebiao@medlinker.com>
2017-07-07 14:06:29 +0800

    absolute path

--- secondtimer.sh
+++ secondtimer.sh
@@ -2,7 +2,7 @@
 while true;
 do
         #需要定时执行的命令，注意结尾的&，进入后台执行，不阻塞
-        /xxxpath/bin/php /yypath/test.php >> /dev/null 2>&1 &
+        /xxpath/bin/php /yypath/test.php >> /dev/null 2>&1 &
         #每秒执行
         sleep 1;
 done
